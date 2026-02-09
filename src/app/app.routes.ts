5d9eb4f6d95f72bad6b554b0797f0d29d6b5d8ad
AMathiasGomez <gomezmathiasxd@gmail.com>
2026-02-09 17:02:14 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './guard/auth-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo:'/auth',
+        pathMatch:'full'
+    },{
+        path:'auth',
+        loadComponent:() => import('./components/auth/auth').then(m => m.Auth),
+        title:"Inicio de sesion"
+    },{
+        path:'chat',
+        loadComponent: () => import('./components/chat/chat').then(m => m.Chat),
+        title:"Chat con tu agente IA",
+        canActivate:[authGuard]
+    },{
+        path:'**',
+        redirectTo:'/auth',
+    }
+];
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,19 @@
+export const enviroment = {
+    production:true,
+    firebaseConfig : {
+        apiKey: "AIzaSyADR4EzqFPJxF7k3CFAq9gg8Y9Vsse53E0",
+        authDomain: "chatangular20.firebaseapp.com",
+        projectId: "chatangular20",
+        storageBucket: "chatangular20.firebasestorage.app",
+        messagingSenderId: "575129338509",
+        appId: "1:575129338509:web:c5aa3a02088e258dfb58c4",
+        measurementId: "G-W249SYMK7B"
+      },
+
+      gemini:{
+        apiUrl:"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent",
+        apiKey:"AIzaSyD1yS0_QFzRE8FzY7NTv7j3sQChRc_ch_A"
+      }
+}
+
+
