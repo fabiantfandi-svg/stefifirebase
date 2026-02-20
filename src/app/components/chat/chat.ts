af5d0c41abed811ec3b85126e29cfed1490a39b1
David-Santiago-Gil <gilsantiagodepa@gmail.com>
2026-02-20 17:01:00 -0500

    diseño de enviando mensaje

--- src/app/components/chat/chat.ts
+++ src/app/components/chat/chat.ts
@@ -1,27 +1,90 @@
 import { Component } from '@angular/core';
 import { MensajeChat } from '../../../models/chat';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-chat',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './chat.html',
   styleUrl: './chat.css',
 })
 export class Chat {
-  
+
   nombre : string ="Fabian David Torres F."
   email : string = "fabiantfandi@gmail.com"
-
   mensajes: MensajeChat[] = []
-
-  cargandoHistorial = 1
-
+  cargandoHistorial = false
+  asistenteEscribiendo = false
+  mensajeTexto = ""
+  enviandoMensaje = false
   manejoErrorImagen() {
 
   }
-
   cerrarSesion(){
 
+  }
+  trackByMensaje(index: number, mensaje: MensajeChat){
+  }
+  formatearMensajeAsistente(mensaje : string){
+
+  }
+
+  ngOnInit(){
+    this.mensajes = this.generarMensajeDemo();
+  }
+  enviarMensaje(){
+
+  }
+
+  private generarMensajeDemo():MensajeChat[]{
+    const ahora = new Date();
+
+    return [
+      {
+        id:'id1',
+        contenido:'Hola eres el asistente?',
+        tipo:'Usuario',
+        fechaEnvio: new Date(ahora.getTime()),
+        estado:'Enviado',
+        usuarioId:'u1'
+      },{
+        id:'id2',
+        contenido:'Hola soy tu asistente',
+        tipo:'Asistente',
+        fechaEnvio: new Date(ahora.getTime()),
+        estado:'Enviado',
+        usuarioId:'a1'
+      },{
+        id:'id3',
+        contenido:'Me puedes resolver una multiplicacion',
+        tipo:'Usuario',
+        fechaEnvio: new Date(ahora.getTime()),
+        estado:'Enviado',
+        usuarioId:'u2'
+      },{
+        id:'id4',
+        contenido:'Claro aqui estoy para ayudarte',
+        tipo:'Asistente',
+        fechaEnvio: new Date(ahora.getTime()),
+        estado:'Enviado',
+        usuarioId:'a2'
+      },{
+        id:'id5',
+        contenido:'Cuanto es 7x87',
+        tipo:'Usuario',
+        fechaEnvio: new Date(ahora.getTime()),
+        estado:'Enviado',
+        usuarioId:'u3'
+      },{
+        id:'id6',
+        contenido:'El resultado de la mulltiplicacion es: 609',
+        tipo:'Asistente',
+        fechaEnvio: new Date(ahora.getTime()),
+        estado:'Enviado',
+        usuarioId:'a3'
+      },
+
+    ]
   }
 
 }
